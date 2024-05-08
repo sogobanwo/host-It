bac0df765a63b39f21468527c598e8cbbf933944
sogobanwo <sogobanwo@gmail.com>
2024-05-08 01:14:28 +0100

    fix: routing issues

--- src/App.js
+++ src/App.js
@@ -16,27 +16,27 @@ const Dashboard = lazy(() => import("./pages/dashboard/dashboard"));
 const Events = lazy(() => import("./pages/events"));
 const MyTickets = lazy(() => import("./pages/dashboard/my-tickets/my-tickets"));
 const Preloader = lazy(() => import("./components/PreLoader"));
-const Registration = lazy(()=> import("./pages/registration"))
+const Registration = lazy(() => import("./pages/registration"))
 
 function App() {
   configureWeb3Modal();
   const { isConnected } = useWeb3ModalAccount();
-  const {loading, data} = useGetENSByAddress()
+  const { loading, data } = useGetENSByAddress()
   return (
     <>
       <ChakraProvider>
         <Suspense fallback={<Preloader />}>
           <Routes>
-            <Route path="/" element={isConnected && data && data[3] === true? <Navigate to="/dashboard" /> : <LandingPage />} />
-            <Route path="/events" element= {isConnected && data && data[3] === true? <Navigate to="/dashboard" /> : <Events />} />
-            <Route path="/dashboard" element={isConnected ? <Dashboard />: <Navigate to="/" /> } />
-            <Route path="/create-event" element={isConnected ?<CreateEvent />: <Navigate to="/" />} />
-            <Route path="/all-events" element={isConnected ?<AllEvents />: <Navigate to="/" />} />
-            <Route path="/all-events/:id" element={isConnected ?<AllEventDetail />: <Navigate to="/" />}/>
-            <Route path="/manage-events" element={isConnected ?<ManageEvents />: <Navigate to="/" />}/>
-            <Route path="/manage-events/:id" element={isConnected ?<ManageEventDetails />: <Navigate to="/" />} />
-            <Route path="/tickets-poap" element={isConnected ?<MyTickets />: <Navigate to="/" />} />
-            <Route path="/ens-registration" element={isConnected && data && data[3] === true ? <Navigate to="/dashboard" /> : <Registration />} />
+            <Route path="/" element={isConnected ? (data && data[3] === true ? <Navigate to="/dashboard" /> : <Navigate to="/ens-registration" />) : <LandingPage />} />
+            <Route path="/events" element={isConnected ? (data && data[3] === true ? <Navigate to="/dashboard" /> : <Navigate to="/ens-registration" />) : <Events />} />
+            <Route path="/dashboard" element={isConnected ? <Dashboard /> : <Navigate to="/" />} />
+            <Route path="/create-event" element={isConnected ? <CreateEvent /> : <Navigate to="/" />} />
+            <Route path="/all-events" element={isConnected ? <AllEvents /> : <Navigate to="/" />} />
+            <Route path="/all-events/:id" element={isConnected ? <AllEventDetail /> : <Navigate to="/" />} />
+            <Route path="/manage-events" element={isConnected ? <ManageEvents /> : <Navigate to="/" />} />
+            <Route path="/manage-events/:id" element={isConnected ? <ManageEventDetails /> : <Navigate to="/" />} />
+            <Route path="/tickets-poap" element={isConnected ? <MyTickets /> : <Navigate to="/" />} />
+            <Route path="/ens-registration" element={isConnected ? (data && data[3] === true ? <Navigate to="/dashboard" /> : <Registration/>) : <LandingPage />} />
           </Routes>
           <Toaster />
         </Suspense>
